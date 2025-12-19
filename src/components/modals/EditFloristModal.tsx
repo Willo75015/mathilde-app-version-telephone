131e27a4b082d93db93d151ca4e099fa3f073977
Willo75015 <wilfried.mendy@gmail.com>
2025-12-19 13:59:00 +0100

    fix: increase z-index and add shadow to sticky footer buttons

--- src/components/modals/EditFloristModal.tsx
+++ src/components/modals/EditFloristModal.tsx
@@ -595,7 +595,7 @@ const EditFloristModal: React.FC<EditFloristModalProps> = ({
         </div>
 
         {/* Actions - Footer fixe sur mobile */}
-        <div className="fixed bottom-0 left-0 right-0 md:relative md:bottom-auto md:left-auto md:right-auto bg-white dark:bg-gray-800 border-t border-gray-200 dark:border-gray-700 p-4 flex justify-end space-x-3 z-50">
+        <div className="fixed bottom-0 left-0 right-0 md:relative md:bottom-auto md:left-auto md:right-auto bg-white dark:bg-gray-800 border-t border-gray-200 dark:border-gray-700 p-4 pb-safe flex justify-end space-x-3 z-[100] shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1)]">
           <Button
             type="button"
             variant="ghost"
